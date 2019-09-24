fe9105e7bb032255aed90253f793a81bf607613c
Caridad Rivera <ccrive10@gmail.com>
2019-09-23 23:52:38 -0400

    almost done

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import background from './background.jpeg'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+          <h1> 2020 </h1>
+          <h6> New Year's Eve Gala! </h6>
+           <div className="narrative">
+              <p>
+                Mirando hacia una nueva decada,
+                terminemos el aNo 2019 de una nueva manera!
+
+                AcompaNenos este 31 de Diciembre a despedir el 2019
+                 con familia y amados!
+
+                 Para reservar porfavor de reservar aqui:
+              </p>
+              <div className="register">
+                <a href="#"> Vamono! </a>
+              </div>
+           </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
